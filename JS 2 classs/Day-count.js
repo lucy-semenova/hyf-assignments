2fd88450c30aec285f0b7ec16e3556ed6a380cc9
Liudmila Siamionava <114390672+lucy-semenova@users.noreply.github.com>
2025-11-19 21:39:29 +0100

    Delete JS 2 classs/Day-count.js

--- JS 2 classs/Day-count.js
+++
@@ -1,9 +0,0 @@
-// Event application
- const dayName = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-function getEventDay(today, daysTillEvent) {
-        
-
-    return dayName[(today+daysTillEvent)%7];
-}
-console.log(getEventDay(0, 1));
