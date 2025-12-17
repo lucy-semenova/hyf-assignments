de86098a9f370912a36b417cb6b984db51e34ff2
Liudmila Siamionava <lucy.semenova@gmail.com>
2025-12-17 12:45:58 +0100

    Fix: update the file.

--- databse_assignment.sql
+++ databse_assignment.sql
@@ -3,19 +3,28 @@ SELECT count (*)
 FROM task; 
 
 -- How many tasks in the task table do not have a valid due date? 
-SELECT * 
-FROM task 
-WHERE due_date IS NULL; 
+SELECT COUNT(*) AS invalid_due_date
+FROM task
+WHERE due_date IS NULL
+   OR due_date = '';
 
 --Find all the tasks that are marked as done. 
 SELECT * 
 FROM task 
-WHERE status_id = 3; 
+WHERE status_id = (
+    SELECT id 
+    FROM status
+    WHERE name = 'Done');
+
 
 -- Find all the tasks that are not marked as done. 
 SELECT * 
 FROM task 
-WHERE status_id != 3 OR status_id IS NULL; 
+WHERE status_id != (
+    SELECT id 
+    FROM status
+    WHERE name = 'Done') 
+	OR status_id IS NULL;
 
 -- Get all the tasks, sorted with the most recently created first. 
 SELECT * 
@@ -34,22 +43,22 @@ FROM task
 WHERE title  LIKE '%database%' OR description  LIKE '%database%'; 
 
 -- Get the title and status (as text) of all tasks. 
-SELECT title, name
+SELECT title, s.name AS name
 FROM task t
 LEFT JOIN status s ON t.status_id = s.id;
 
 -- Get the name of each status, along with a count of how many tasks have that status. 
-
 SELECT name,
 COUNT(status_id) AS Number
-FROM task
-RIGHT JOIN Status ON task.status_id = status.id 
-GROUP BY status_id;
+FROM status s
+LEFT JOIN task t ON t.status_id = s.id 
+GROUP BY t.status_id;
+
 
 -- Get the names of all statuses, sorted by the status with most tasks first. 
-SELECT name, 
-COUNT(status_id) AS Number 
-FROM task
-RIGHT JOIN Status ON task.status_id = status.id 
-GROUP BY status_id
+SELECT s.name, 
+COUNT(t.status_id) AS Number 
+FROM status s
+LEFT JOIN task t ON t.status_id = s.id 
+GROUP BY t.status_id
 ORDER BY Number DESC;
