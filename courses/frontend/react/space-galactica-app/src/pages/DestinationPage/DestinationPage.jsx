48899674e774d346ef64e74221d220022a8b1964
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-04-27 14:18:49 +0200

    feat(destinations): create PlanetsWishlistItem component and import into page

--- courses/frontend/react/space-galactica-app/src/pages/DestinationPage/DestinationPage.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/DestinationPage/DestinationPage.jsx
@@ -1,17 +1,8 @@
 import { useState } from "react";
 import styles from "./DestinationPage.module.css";
+import PlanetsWishlistItem from "./PlanetsWishlistItem";
 
-// 🧑🏽‍🚀 Task - Week 2
-// Move this to its own file in this folder.
-const PlanetsWishlistItem = ({ name, thumbnail, onRemove }) => {
-  return (
-    <div className={styles.wishlistItem}>
-      <img className={styles.wishlistItemThumbnail} src={thumbnail} alt="" />
-      <b>{name.toUpperCase()}</b>
-      <button onClick={onRemove}>remove</button>
-    </div>
-  );
-};
+
 
 export const Destinations = () => {
   const [planetsWishlist, setPlanetsWishlist] = useState([]);
