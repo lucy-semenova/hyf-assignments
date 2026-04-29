89983e520039d45adfb04492e093209d499f99ac
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-04-29 09:01:49 +0200

    feat(EventCard): pass event data to EventCard via props

--- courses/frontend/react/events-startup-project/templates/app/app-vite/src/components/EventCard/EventCard.jsx
+++ courses/frontend/react/events-startup-project/templates/app/app-vite/src/components/EventCard/EventCard.jsx
@@ -1,14 +1,14 @@
 import "./EventCard.css";
 
-export default function EventCard() {
+export default function EventCard(title, date, venue, type, price, ticketsLeft) {
   return (
     <li className="eventCard">
-      <h2 className="eventTitle">Aarhus AI @ Uber</h2>
-      <p className="eventDate">May 19, 2026 at 16:00</p>
-      <p className="eventVenue">Carl Blochs Gade 89, Aarhus</p>
-      <p className="eventType">Technology</p>
-      <p className="eventPrice">Free</p>
-      <p className="eventTicketsLeft">24 tickets left</p>
+      <h2 className="eventTitle">{title}</h2>
+      <p className="eventDate">{date}</p>
+      <p className="eventVenue">{venue}</p>
+      <p className="eventType">{type}</p>
+      <p className="eventPrice">{price === 0 ? "Free" : `${price} DKK`}</p>
+      <p className="eventTicketsLeft">{ticketsLeft} tickets left</p>
       <button className="buyBtn">Buy ticket</button>
     </li>
   );
