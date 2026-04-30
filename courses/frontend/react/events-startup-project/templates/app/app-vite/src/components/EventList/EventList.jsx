9ee14cf63c49048282602f63c292a19e6d1a6d28
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-04-30 11:37:41 +0200

    feat(EventList): add search input for filtering events

--- courses/frontend/react/events-startup-project/templates/app/app-vite/src/components/EventList/EventList.jsx
+++ courses/frontend/react/events-startup-project/templates/app/app-vite/src/components/EventList/EventList.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import EventCard from "../EventCard/EventCard";
 import events from "../../data/events.js";
 
- function EventList() {
+
+function EventList() {
+  const [searchQuery, setSearchQuery] = useState("");
+  
+ const filteredEvents = events.filter((event) =>
+    event.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <section>
       <h1>Upcoming Events</h1>
 
-      
-      {events.length === 0 ? (
+  <input
+        type="text"
+        placeholder="Search events..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      {filteredEvents.length === 0 ? (
         <p className="noEvents">No events available.</p>
       ) : (
         <ul>
-          {events.map((event) => (
+          {filteredEvents.map((event) => (
             <EventCard
               key={event.id}
               title={event.title}
